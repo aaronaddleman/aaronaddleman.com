728ecb9a69c7a978e1d6bf668783691519e2cf6e
Aaron Addleman <aaronaddleman@gmail.com>
2013-08-23 07:55:51 -0700

    setup capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,32 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "aaronaddleman.com"
+set :repository,  "git@www.squaron.net:aaronaddleman"
+set :scm, :git
+set :keep_releases, 15
+set :app_root, "wordpress"
+set :deploy_via, :remote_cache
+ssh_options[:user] = 'deployer'
+set :use_sudo, false
+default_run_options[:pty] = true
+set :remote_user, 'deployer'
+set :deploy_to, "/apps/#{application}"
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, "www.squaron.net"                          # Your HTTP server, Apache/etc
+role :app, "www.squaron.net"                          # This may be the same as your `Web` serve r
+# role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+# role :db,  "your slave db-server here"
 
 # if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+after "deploy:restart", "deploy:cleanup"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
