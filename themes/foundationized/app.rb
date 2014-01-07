be4029ce5e7e35b855305a31ef0645380a0a6dbc
aaron addleman <aaronaddleman@gmail.com>
2014-01-07 08:25:51 -0800

    add files

--- themes/foundationized/app.rb
+++ themes/foundationized/app.rb
@@ -15,5 +15,9 @@ module Nesta
     end
 
     # Add new routes here.
+    get '/css/:sheet.css' do
+      content_type 'text/css', :charset => 'utf-8'
+      cache scss(params[:sheet].to_sym)
+    end
   end
 end
