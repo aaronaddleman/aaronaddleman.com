2d32ed94b17c30922ca20cd40e5b41c9167439f5
aaron addleman <aaronaddleman@gmail.com>
2014-01-27 14:35:29 -0800

    made changes to everything to look like old nesta theme but with foundation code
    this commit includes so many changes:
    -subtitle change
    -layout updates
    -pagination cleanup with nokogiri

--- themes/foundationized/app.rb
+++ themes/foundationized/app.rb
@@ -10,8 +10,24 @@ module Nesta
     #
     use Rack::Static, :urls => ["/foundationized"], :root => "themes/foundationized/public"
 
+    def foundation_pager(str)
+      @doc = Nokogiri::XML::DocumentFragment.parse(str)
+      @doc.search('div').map { |e| e.node_name = "ul" }
+      @doc.css('a').wrap("<li></li>")
+      @doc.css('span[class="gap"]').wrap("<li class=\"unavailable\"></li>")
+      @doc.search("span[class='gap']").map { |e| e.node_name = "a"; e.content = "..."}
+      @doc.css('em').wrap("<li class=\"current\"></li>")
+      @doc.search('./ul/li/em').map {|e| e.node_name = "a"}
+      @doc.css('span[class="previous_page disabled"]').wrap("<li class=\"unavailable\"></li>")
+      @doc.search('./ul/li/span').map {|e| e.node_name = "a"}
+      @doc.to_html
+    end
+
     helpers do
       # Add new helpers here.
+      def pages_heading
+        @page.metadata('projects heading') || "Pages on #{@page.heading}"
+      end
     end
 
     # Add new routes here.
