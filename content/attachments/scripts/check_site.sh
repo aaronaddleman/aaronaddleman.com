472bd6895f811f35f15aaefd4e6e37e02a0914ab
aaron addleman <aaronaddleman@gmail.com>
2014-03-27 22:40:32 -0700

    change check_site.sh to email results

--- content/attachments/scripts/check_site.sh
+++ content/attachments/scripts/check_site.sh
@@ -1,2 +1,2 @@
 WGET=`which wget`
-${WGET} --spider -o ./check_site.log -e robots=off -r -p http://aaronaddleman.com
+${WGET} --spider -e robots=off -r -p http://aaronaddleman.com 2>&1 | mail -s "Spider results for aaronaddleman.com" aaronaddleman@gmail.com
